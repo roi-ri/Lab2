2a0e29ef94ae1ff27b910e94fdeb088e8b143949
roi-ri <rodrigo.e.sanchez.araya@gmail.com>
2024-04-13 14:46:30 -0600

    se estuvo verificando el funcionamiento en ciertos casos del archivo y corrigiendo pequenos detalles

--- ejercicio4.sh
+++ ejercicio4.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# Directorio a monitorear
-directorio_monitoreado="/home"
+# Directorio a monitorear 
+directorio_monitoreado="/home/roiri/Lab2/"
 
 # Archivo de log para registrar los eventos de cambios
-archivo_log="/home/roiri/log_lab2/datos.log"
+archivo_log="/home/roiri/log_lab2/datos1.log"
 
 # Mensaje de log con la fecha y hora del cambio
 log_message() {
     echo "$(date +'%Y-%m-%d %H:%M:%S') - Cambio detectado en $1: $2" >> "$archivo_log"
 }
 
-# Monitorear el directorio en busca de cambios continuamente
+# Monitorear los directorios en busca de cambios continuamente
 inotifywait -m -r -e create,modify,delete "$directorio_monitoreado" | while read evento; do
     log_message "$directorio_monitoreado" "$evento"
 done
+
