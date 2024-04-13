ca716177c5ab3458155d7abef9fab18661e6645a
roi-ri <rodrigo.e.sanchez.araya@gmail.com>
2024-04-13 14:01:25 -0600

    Se agrega el cuarto ejercicio, ademas un ejemplo del archivo creado bajo el nombre monitoreo.service, este se encuentra en el directorio de /etc/systemd/system/monitoreo.service, dentro del reporte se muestra la ejecucion del mismo

--- ejercicio4.sh
+++ ejercicio4.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 # Directorio a monitorear
-DIRECTORIO="/home/user/documents"
+directorio_monitoreado="/home"
 
-# Archivo de log
-LOG_FILE="/var/log/directory_changes.log"
+# Archivo de log para registrar los eventos de cambios
+archivo_log="/home/roiri/log_lab2/datos.log"
 
-# Ejecutar inotifywait para monitorear el directorio
-inotifywait -m -r -e create,modify,delete --format "%T %w %e %f" "$DIRECTORIO" >> "$LOG_FILE" 2>&1
+# Mensaje de log con la fecha y hora del cambio
+log_message() {
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - Cambio detectado en $1: $2" >> "$archivo_log"
+}
+
+# Monitorear el directorio en busca de cambios continuamente
+inotifywait -m -r -e create,modify,delete "$directorio_monitoreado" | while read evento; do
+    log_message "$directorio_monitoreado" "$evento"
+done
